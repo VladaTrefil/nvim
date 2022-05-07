0b4ba786e2e8e248210af66e6fdcdc5293842ec9
Vlada <vladislav.trefil@protonmail.com>
2022-05-07 21:34:07 +0200

    vim - fix lualine bubbles

--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -66,14 +66,14 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 4 },
+      { 'mode', separator = { left = '', right = '' }, right_padding = 4 },
     },
     lualine_b = { file_path, 'branch' },
     lualine_c = { 'fileformat' },
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
     lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 4 },
+      { 'location', separator = { left = '', right = '' }, left_padding = 4 },
     },
   },
   inactive_sections = {
