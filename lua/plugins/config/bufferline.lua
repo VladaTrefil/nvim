ab32b022bab2b7f1a0bff6220aff002a82c83a6a
Vlada <vladislav.trefil@protonmail.com>
2023-08-18 01:40:39 +0200

    nvim - show only current windows in bufferline

--- lua/plugins/config/bufferline.lua
+++ lua/plugins/config/bufferline.lua
@@ -4,4 +4,20 @@ if not present then
   return
 end
 
-bufferline.setup({})
+-- show only buffers with window open on current tab
+local tab_buffers_filter = function(bufnr)
+  local winid = vim.fn.win_findbuf(bufnr)[1]
+  local tabnr = vim.fn.win_id2tabwin(winid)[1]
+
+  local current_tabnr = vim.fn.tabpagenr()
+
+  if tabnr == current_tabnr then
+    return true
+  end
+end
+
+bufferline.setup({
+  options = {
+    custom_filter = tab_buffers_filter,
+  },
+})
