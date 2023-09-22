4314863162ebc344eb9b836a67b4721f962707f4
Vlada <vladislav.trefil@protonmail.com>
2023-09-22 02:01:00 +0200

    codespell - add config and alias

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -1,12 +1,14 @@
-vim.cmd("highlight clear")
+vim.cmd('highlight clear')
 
-if vim.fn.exists "syntax_on" then vim.cmd("syntax reset") end
+if vim.fn.exists('syntax_on') then
+  vim.cmd('syntax reset')
+end
 
-vim.o.background = "dark"
+vim.o.background = 'dark'
 vim.o.termguicolors = true
 
 local highlight_groups = {
-  base = { "base", "highlight_groups" },
+  base = { 'base', 'highlight_groups' },
   plugins = { 'telescope', 'lualine', 'easymotion', 'cmp', 'gitgutter', 'indentline', 'bufferline' },
   syntax = { 'base', 'lua', 'vim', 'javascript', 'java', 'json', 'ruby', 'xml', 'css', 'html', 'markdown', 'rasi' },
 }
@@ -14,15 +16,15 @@ local highlight_groups = {
 local highlights = {}
 
 for _, module in ipairs(highlight_groups.base) do
-  highlights = vim.tbl_deep_extend("force", highlights, require("theme." .. module))
+  highlights = vim.tbl_deep_extend('force', highlights, require('theme.' .. module))
 end
 
 for _, module in ipairs(highlight_groups.plugins) do
-  highlights = vim.tbl_deep_extend("force", highlights, require("theme.plugins." .. module))
+  highlights = vim.tbl_deep_extend('force', highlights, require('theme.plugins.' .. module))
 end
 
 for _, module in ipairs(highlight_groups.syntax) do
-  highlights = vim.tbl_deep_extend("force", highlights, require("theme.syntax." .. module))
+  highlights = vim.tbl_deep_extend('force', highlights, require('theme.syntax.' .. module))
 end
 
 for group, spec in pairs(highlights) do
