b56f1eb4d3cefad5b1888b1ec7551e0bcba6b975
Vlada <vladislav.trefil@protonmail.com>
2023-10-03 23:41:42 +0200

    nvim - add notify and undo mapping

--- lua/plugins/config/_telescope/mappings.lua
+++ lua/plugins/config/_telescope/mappings.lua
@@ -1,93 +1,100 @@
 local M = {}
 
 M.base = {
-  n = {
-    ['<leader>pm'] = { '<cmd> Telescope <CR>', 'Menu [telescope]' },
-    ['<leader>pp'] = {
-      '<cmd> Telescope find_files follow=true hidden=true <CR>',
-      'Search in project',
-    },
-    ['<leader>pw'] = {
-      '<cmd> Telescope live_grep <CR>',
-      'Search in project by pattern [telescope]',
-    },
-    ['<leader>pb'] = { '<cmd> Telescope buffers <CR>', 'Buffers [telescope]' },
-    ['<leader>ph'] = { '<cmd> Telescope help_tags <CR>', 'Help page [telescope]' },
-    ['<leader>po'] = { '<cmd> Telescope oldfiles <CR>', 'Recent files [telescope]' },
+	n = {
+		['<leader>pm'] = { '<cmd> Telescope <CR>', 'Menu [telescope]' },
+		['<leader>pp'] = {
+			'<cmd> Telescope find_files follow=true hidden=true <CR>',
+			'Search in project',
+		},
+		['<leader>pw'] = {
+			'<cmd> Telescope live_grep <CR>',
+			'Search in project by pattern [telescope]',
+		},
+		['<leader>pb'] = { '<cmd> Telescope buffers <CR>', 'Buffers [telescope]' },
+		['<leader>ph'] = { '<cmd> Telescope help_tags <CR>', 'Help page [telescope]' },
+		['<leader>po'] = { '<cmd> Telescope oldfiles <CR>', 'Recent files [telescope]' },
+		['<leader>pu'] = { '<cmd> Telescope undo <CR>', 'Vim options [telescope]' },
 
-    -- -- vim internals
-    ['<leader>pvk'] = { '<cmd> Telescope keymaps <CR>', 'Keymaps [telescope]' },
-    ['<leader>pvm'] = { '<cmd> Telescope man_pages <CR>', 'Man pages [telescope]' },
-    ['<leader>pvn'] = { '<cmd> Telescope notify <CR>', 'Notifications [telescope]' },
-    ['<leader>pvc'] = {
-      '<cmd> Telescope command_history <CR>',
-      'Command history [telescope]',
-    },
-    ['<leader>pvr'] = {
-      '<cmd> Telescope reloader <CR>',
-      'Lua module reloader [telescope]',
-    },
-    ['<leader>pvh'] = { '<cmd> Telescope highlights <CR>', 'Highlights [telescope]' },
-    ['<leader>pva'] = {
-      '<cmd> Telescope autocommands <CR>',
-      'Autocommands [telescope]',
-    },
-    ['<leader>pvo'] = { '<cmd> Telescope vim_options <CR>', 'Vim options [telescope]' },
+		-- -- vim internals
+		['<leader>pvk'] = { '<cmd> Telescope keymaps <CR>', 'Keymaps [telescope]' },
+		['<leader>pvm'] = { '<cmd> Telescope man_pages <CR>', 'Man pages [telescope]' },
+		['<leader>pvn'] = {
+			function()
+				require('telescope').extensions.notify.notify({ layout_strategy = 'vertical' })
+			end,
+			'Notifications [telescope]',
+		},
 
-    -- -- git
-    ['<leader>gc'] = { '<cmd> Telescope git_commits <CR>', 'git commits [telescope]' },
-    ['<leader>gs'] = { '<cmd> Telescope git_status <CR>', 'git status [telescope]' },
-  },
+		['<leader>pvc'] = {
+			'<cmd> Telescope command_history <CR>',
+			'Command history [telescope]',
+		},
+		['<leader>pvr'] = {
+			'<cmd> Telescope reloader <CR>',
+			'Lua module reloader [telescope]',
+		},
+		['<leader>pvh'] = { '<cmd> Telescope highlights <CR>', 'Highlights [telescope]' },
+		['<leader>pva'] = {
+			'<cmd> Telescope autocommands <CR>',
+			'Autocommands [telescope]',
+		},
+		['<leader>pvo'] = { '<cmd> Telescope vim_options <CR>', 'Vim options [telescope]' },
+
+		-- -- git
+		['<leader>gc'] = { '<cmd> Telescope git_commits <CR>', 'git commits [telescope]' },
+		['<leader>gs'] = { '<cmd> Telescope git_status <CR>', 'git status [telescope]' },
+	},
 }
 
 local actions = require('telescope.actions')
 
 M.window = {
-  n = {
-    ['q'] = actions.close,
-    ['<C-k>'] = actions.move_selection_previous,
-    ['<C-j>'] = actions.move_selection_next,
-    ['<C-s>'] = actions.file_split,
-  },
-  i = {
-    ['<C-k>'] = actions.move_selection_previous,
-    ['<C-j>'] = actions.move_selection_next,
-    ['<C-s>'] = actions.file_split,
-    ['<C-p>'] = false,
-    ['<C-n>'] = false,
-  },
+	n = {
+		['q'] = actions.close,
+		['<C-k>'] = actions.move_selection_previous,
+		['<C-j>'] = actions.move_selection_next,
+		['<C-s>'] = actions.file_split,
+	},
+	i = {
+		['<C-k>'] = actions.move_selection_previous,
+		['<C-j>'] = actions.move_selection_next,
+		['<C-s>'] = actions.file_split,
+		['<C-p>'] = false,
+		['<C-n>'] = false,
+	},
 }
 
 local telescope_utils = require('plugins.config._telescope.utils')
 
 M.pickers = {
-  find_files = {
-    -- n = {
-    --   ['<C-t>'] = select_file_entry,
-    -- },
-    -- i = {
-    --   ['<C-t>'] = select_file_entry,
-    -- }
-  },
-  buffers = {
-    n = {
-      ['<enter>'] = telescope_utils.select_buffer,
-      ['d'] = actions.delete_buffer,
-    },
-    i = {
-      ['<enter>'] = telescope_utils.select_buffer,
-    },
-  },
-  help_tags = {
-    -- n = {
-    --   ['<C-v>'] = actions.select_vertical,
-    --   ['<C-s>'] = actions.select_horizontal,
-    -- },
-    --   i = {
-    --     ['<C-v>'] = actions.file_vsplit,
-    --     ['<C-s>'] = actions.file_split,
-    --   }
-  },
+	find_files = {
+		-- n = {
+		--   ['<C-t>'] = select_file_entry,
+		-- },
+		-- i = {
+		--   ['<C-t>'] = select_file_entry,
+		-- }
+	},
+	buffers = {
+		n = {
+			['<enter>'] = telescope_utils.select_buffer,
+			['d'] = actions.delete_buffer,
+		},
+		i = {
+			['<enter>'] = telescope_utils.select_buffer,
+		},
+	},
+	help_tags = {
+		-- n = {
+		--   ['<C-v>'] = actions.select_vertical,
+		--   ['<C-s>'] = actions.select_horizontal,
+		-- },
+		--   i = {
+		--     ['<C-v>'] = actions.file_vsplit,
+		--     ['<C-s>'] = actions.file_split,
+		--   }
+	},
 }
 
 return M
--- lua/plugins/config/_telescope/utils.lua
+++ lua/plugins/config/_telescope/utils.lua
@@ -9,47 +9,51 @@ local action_utils = require('telescope.actions.utils')
 local actions = require('telescope.actions')
 
 M.create_autocmds = function()
-  -- Prevent telescope from opening files in insert mode
-  -- See: https://github.com/nvim-telescope/telescope.nvim/issues/2027
-  vim.api.nvim_create_autocmd('WinLeave', {
-    callback = function()
-      if vim.bo.ft == 'TelescopePrompt' and vim.fn.mode() == 'i' then
-        vim.api.nvim_feedkeys(
-          vim.api.nvim_replace_termcodes('<Esc>', true, false, true),
-          'i',
-          false
-        )
-      end
-    end,
-  })
+	-- Set telescope previewer buffer local options
+	vim.api.nvim_create_autocmd('User', {
+		pattern = 'TelescopePreviewerLoaded',
+		callback = function(args)
+			vim.opt_local.wrap = true
+			vim.opt_local.list = true
+			vim.opt_local.tabstop = 2
+			vim.opt_local.shiftwidth = 2
+			vim.opt_local.number = true
 
-  -- Set telescope previewer buffer local options
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'TelescopePreviewerLoaded',
-    callback = function()
-      vim.opt_local.wrap = true
-      vim.opt_local.number = true
-      vim.opt_local.list = true
-      vim.opt_local.tabstop = 2
-      vim.opt_local.shiftwidth = 2
-    end,
-  })
+			-- if action_state.get_current_picker(args.bufnr) then
+			-- 	vim.opt_local.number = true
+			-- end
+		end,
+	})
+
+	-- Prevent telescope from opening files in insert mode
+	-- See: https://github.com/nvim-telescope/telescope.nvim/issues/2027
+	vim.api.nvim_create_autocmd('WinLeave', {
+		callback = function()
+			if vim.bo.ft == 'TelescopePrompt' and vim.fn.mode() == 'i' then
+				vim.api.nvim_feedkeys(
+					vim.api.nvim_replace_termcodes('<Esc>', true, false, true),
+					'i',
+					false
+				)
+			end
+		end,
+	})
 end
 
 M.load_extension = function(name)
-  telescope.load_extension(name)
+	telescope.load_extension(name)
 end
 
 -- Switch to window with buffer if exists or do default select action
 M.select_buffer = function(prompt_bufnr)
-  local entry = action_state.get_selected_entry()
-  local winid = vim.fn.win_findbuf(entry.bufnr)[1]
+	local entry = action_state.get_selected_entry()
+	local winid = vim.fn.win_findbuf(entry.bufnr)[1]
 
-  if winid then
-    vim.fn.win_gotoid(winid)
-  else
-    actions.select_default(prompt_bufnr)
-  end
+	if winid then
+		vim.fn.win_gotoid(winid)
+	else
+		actions.select_default(prompt_bufnr)
+	end
 end
 
 -- local function select_file_entry(prompt_bufnr)
