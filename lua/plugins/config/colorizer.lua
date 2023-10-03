52401871c047c9c59d5509716130a666ad17c50c
Vlada <vladislav.trefil@protonmail.com>
2023-10-04 00:52:15 +0200

    add colorizer

--- lua/plugins/config/colorizer.lua
+++ lua/plugins/config/colorizer.lua
@@ -1,30 +1,40 @@
 local colorizer_ok, colorizer = pcall(require, 'colorizer')
 
 if not colorizer_ok then
-  return
+	return
 end
 
-local options = {
-  filetypes = {
-    '*',
-  },
+local autocmd_group = 'Colorizer'
 
-  user_default_options = {
-    RGB = true, -- #RGB hex codes
-    RRGGBB = true, -- #RRGGBB hex codes
-    names = false, -- "Name" codes like Blue
-    RRGGBBAA = false, -- #RRGGBBAA hex codes
-    rgb_fn = false, -- CSS rgb() and rgba() functions
-    hsl_fn = false, -- CSS hsl() and hsla() functions
-    css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-    mode = 'background', -- Set the display mode.
-  },
+local general_options = {
+	RGB = true, -- #RGB hex codes
+	RRGGBB = true, -- #RRGGBB hex codes
+	RRGGBBAA = true, -- #RRGGBBAA hex codes
+	names = false, -- "Name" codes like Blue
+	mode = 'background', -- Set the display mode.
 }
 
-colorizer.setup(options)
+local filetype_options = {
+	filetypes = {
+		'*',
+		css = {
+			rgb_fn = true, -- CSS rgb() and rgba() functions
+			hsl_fn = true, -- CSS hsl() and hsla() functions
+			css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+			css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+		},
+	},
+}
 
--- execute colorizer as soon as possible
-vim.defer_fn(function()
-  require('colorizer').attach_to_buffer(0)
-end, 0)
+colorizer.setup(filetype_options, general_options)
+
+vim.api.nvim_create_augroup(autocmd_group, { clear = true })
+vim.api.nvim_create_autocmd('BufRead', {
+	group = autocmd_group,
+	pattern = '*',
+	callback = function()
+		vim.defer_fn(function()
+			require('colorizer').attach_to_buffer(0)
+		end, 0)
+	end,
+})
