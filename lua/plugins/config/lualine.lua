c52c83b88f3e7f2b69a321a725575076e9f00162
Vlada <vladislav.trefil@protonmail.com>
2023-10-05 01:25:56 +0200

    nvim - tweak lualine

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -1,92 +1,94 @@
 local present, lualine = pcall(require, 'lualine')
 
 if not present then
-  return
+	return
 end
 
-local palette = {}
-
-palette = {
-  bg0 = '#0B0E14',
-  bg1 = '#0D1017',
-  bg2 = '#151f28',
-  fg0 = '#E6E1CF',
-  fg1 = '#E6E1CF',
-  yellow = '#E7C547',
-  orange = '#F29718',
-  aqua = '#1f997e',
-  bright_red = '#e23141',
-}
+local colors = require('theme.colors')
 
 local function file_path()
-  local path = vim.fn.expand('%f')
-  local _, count = path:gsub('/', '')
+	local path = vim.fn.expand('%f')
+	local _, count = path:gsub('/', '')
 
-  if count > 5 then
-    local path_ary = vim.fn.split('/')
+	if count > 3 then
+		local path_ary = vim.fn.split(path, '/')
 
-    -- return string.rep(".", count)
-    return path
-  else
-    return path
-  end
+		for index, value in ipairs(path_ary) do
+			local diff = count - index
+
+			if diff >= 3 then
+				path_ary[index] = value:sub(1, 1)
+			elseif diff >= 1 then
+				path_ary[index] = value:sub(1, 3)
+			else
+				path_ary[index] = value
+			end
+		end
+
+		return vim.fn.join(path_ary, '/')
+	else
+		return path
+	end
 end
 
 local bubbles_theme = {
-  normal = {
-    a = { fg = palette.bg0, bg = palette.aqua },
-    b = { fg = palette.fg1, bg = palette.bg2 },
-    c = { fg = palette.bg0, bg = palette.bg0 },
-  },
+	normal = {
+		a = { fg = colors.dark0, bg = colors.bright_blue },
+		b = { fg = colors.light0, bg = colors.dark2 },
+		c = { fg = colors.light1, bg = colors.dark1 },
+		z = { fg = colors.dark0, bg = colors.bright_blue },
+	},
 
-  insert = { a = { fg = palette.bg0, bg = palette.yellow } },
-  visual = { a = { fg = palette.fg0, bg = palette.bright_red } },
-  replace = { a = { fg = palette.bg0, bg = palette.orange } },
+	insert = { a = { fg = colors.dark0, bg = colors.bright_yellow } },
+	visual = { a = { fg = colors.light0, bg = colors.bright_red } },
+	replace = { a = { fg = colors.dark0, bg = colors.bright_orange } },
 
-  inactive = {
-    a = { fg = palette.fg1, bg = palette.bg2 },
-    b = { fg = palette.fg1, bg = palette.bg0 },
-    c = { fg = palette.bg2, bg = palette.bg0 },
-  },
+	inactive = {
+		a = { fg = colors.light3, bg = colors.dark2 },
+		b = { fg = colors.light3, bg = colors.dark0 },
+		c = { fg = colors.gray, bg = colors.dark0 },
+	},
 }
 
 lualine.setup({
-  options = {
-    theme = bubbles_theme,
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-    padding = { left = 1, right = 1 },
-  },
-  sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '', right = '' } },
-    },
-    lualine_b = {
-      { file_path, padding = { left = 2, right = 2 } },
-      { 'branch', padding = { left = 2, right = 2 } },
-    },
-    lualine_c = { 'fileformat' },
-    lualine_x = {},
-    lualine_y = {
-      { 'filetype' },
-      { 'progress' },
-    },
-    lualine_z = {
-      { 'location', separator = { left = '', right = '' }, padding = { left = 0, right = 1 } },
-    },
-  },
-  inactive_sections = {
-    lualine_a = {
-      { file_path, separator = { left = '', right = '' } },
-    },
-    -- lualine_b = {},
-    -- lualine_c = {},
-    -- lualine_x = {},
-    -- lualine_y = {},
-    lualine_z = {
-      { 'filetype', padding = { right = 2, left = 2 } },
-    },
-  },
-  tabline = {},
-  extensions = {},
+	options = {
+		theme = bubbles_theme,
+		component_separators = '|',
+		section_separators = { left = '', right = '' },
+		padding = { left = 1, right = 1 },
+		disabled_filetypes = {
+			statusline = {
+				'alpha',
+			},
+		},
+	},
+	sections = {
+		lualine_b = {
+			{ file_path, padding = { left = 2, right = 2 } },
+			{ 'branch', padding = { left = 2, right = 2 } },
+		},
+		lualine_c = { 'fileformat' },
+		lualine_x = {},
+		lualine_y = {
+			{ 'filetype' },
+			{ 'progress' },
+		},
+		lualine_z = {
+			{
+				'location',
+				padding = { left = 1, right = 1 },
+			},
+		},
+	},
+	inactive_sections = {
+		-- lualine_b = {},
+		-- lualine_c = {},
+		-- lualine_x = {},
+		-- lualine_y = {},
+		lualine_z = {
+			{ 'filetype', padding = { right = 2, left = 2 } },
+		},
+	},
+	tabline = {},
+	extensions = {},
 })
