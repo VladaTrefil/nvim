2381db96de0aad38684b56a3ac1d2498154bc3af
Vlada <vladislav.trefil@protonmail.com>
2023-11-17 01:18:21 +0100

    cleanup

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -1,48 +1,48 @@
 local treesitter_ok, treesitter = pcall(require, 'nvim-treesitter')
 
 if not treesitter_ok then
-  return
+	return
 end
 
 local FILETYPES = {
-  'git_config',
-  'vimdoc',
-  'lua',
-  'javascript',
-  'typescript',
-  'c',
-  'rust',
-  'ruby',
-  'python',
-  'yaml',
-  'bash',
-  'scss',
+	'git_config',
+	'vimdoc',
+	'lua',
+	'javascript',
+	'typescript',
+	'c',
+	'rust',
+	'ruby',
+	'python',
+	'yaml',
+	'bash',
+	'scss',
 }
 
 require('nvim-treesitter.configs').setup({
-  -- A list of parser names, or "all"
-  ensure_installed = FILETYPES,
+	-- A list of parser names, or "all"
+	ensure_installed = FILETYPES,
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
 
-  -- TODO: Indent is usually wrong, see
-  -- https://github.com/nvim-treesitter/nvim-treesitter/issues/2507
-  indent = {
-    enable = false,
-  },
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
+	-- TODO: Indent is usually wrong, see
+	-- https://github.com/nvim-treesitter/nvim-treesitter/issues/2507
+	indent = {
+		enable = false,
+	},
+	highlight = {
+		-- `false` will disable the whole extension
+		enable = true,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
 })
