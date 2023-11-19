e267c414f103bf05973f5a20d4c9a7648b57193c
Vlada <vladislav.trefil@protonmail.com>
2023-11-19 02:25:46 +0100

    nvim - tweak autopairs

--- lua/plugins/config/autopairs.lua
+++ lua/plugins/config/autopairs.lua
@@ -10,16 +10,18 @@ autopairs.setup({
 	disable_filetype = { 'TelescopePrompt', 'vim' },
 	ts_config = {
 		lua = { 'string' },
-		javascript = { 'template_string' },
+		javascript = { 'string', 'template_string' },
 		java = false,
+		ruby = { 'string' },
+		slim = { 'string' },
 	},
 })
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp_ok, cmp = pcall(require, 'cmp')
 
-if not (cmp_ok or cmp_autopairs) then
+if not cmp_ok then
 	return
 end
 
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
