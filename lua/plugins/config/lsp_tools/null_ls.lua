aebdae594e87c840b0c95e5bb1714ba6979d8460
Vlada <vladislav.trefil@protonmail.com>
2023-11-20 02:35:17 +0100

    nvim - disable codespell

--- lua/plugins/config/lsp_tools/null_ls.lua
+++ lua/plugins/config/lsp_tools/null_ls.lua
@@ -18,18 +18,18 @@ local sources_config = {
 			vim.fn.expand('$XDG_CONFIG_HOME/stylelint/stylelintrc.json'),
 		},
 	},
-	codespell = {
-		extra_args = {
-			'--ignore-words',
-			vim.fn.expand('$XDG_CONFIG_HOME/codespell/ignore.txt'),
-			'--exclude-file',
-			vim.fn.expand('$XDG_CONFIG_HOME/codespell/exclude-file.txt'),
-			'--config',
-			vim.fn.expand('$XDG_CONFIG_HOME/codespell/codespellrc'),
-			'--regex',
-			"(?<![a-z])[a-z'`]+|[A-Z][a-z'`]*|[a-z]+'[a-z]*|[a-z]+(?=[_-])|[a-z]+(?=[A-Z])|\\d+",
-		},
-	},
+	-- codespell = {
+	-- 	extra_args = {
+	-- 		'--ignore-words',
+	-- 		vim.fn.expand('$XDG_CONFIG_HOME/codespell/ignore.txt'),
+	-- 		'--exclude-file',
+	-- 		vim.fn.expand('$XDG_CONFIG_HOME/codespell/exclude-file.txt'),
+	-- 		'--config',
+	-- 		vim.fn.expand('$XDG_CONFIG_HOME/codespell/codespellrc'),
+	-- 		'--regex',
+	-- 		"(?<![a-z])[a-z'`]+|[A-Z][a-z'`]*|[a-z]+'[a-z]*|[a-z]+(?=[_-])|[a-z]+(?=[A-Z])|\\d+",
+	-- 	},
+	-- },
 	shellcheck = {
 		diagnostic_config = {
 			underline = true,
