d7061521e8ffadd84369e46819cc8bd58f17753b
Vlada <vladislav.trefil@protonmail.com>
2023-12-29 23:07:37 +0100

    fix(nvim): telescope user event invalid 'telescopefindpre'

--- lua/plugins/plugin_list.lua
+++ lua/plugins/plugin_list.lua
@@ -55,7 +55,7 @@ return {
 	},
 
 	['nvim-telescope/telescope.nvim'] = {
-		tag = '0.1.0',
+		tag = '0.1.4',
 		requires = {
 			'nvim-lua/plenary.nvim',
 			'nvim-telescope/telescope-live-grep-args.nvim',
