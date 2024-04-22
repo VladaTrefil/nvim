2560fa18bcff3da16c8e7dfdeefdedc683d1e1aa
Vlada <vladislav.trefil@protonmail.com>
2024-04-22 02:57:14 +0200

    fix(nvim): conform rubocop path and settings

--- lua/plugins/config/_conform/init.lua
+++ lua/plugins/config/_conform/init.lua
@@ -5,7 +5,7 @@ if not conform_ok then
 	return
 end
 
-local ignore_filetypes = { 'javascript', 'sass' }
+local ignore_filetypes = { 'sass' }
 local disable_autoformat = false
 
 local rubocop = require('plugins.config._conform.rubocop')
@@ -34,6 +34,37 @@ local autoformat_opts = {
 
 vim.g.disable_autoformat = disable_autoformat
 
+local format_on_save = function(bufnr)
+	local bufname = vim.api.nvim_buf_get_name(bufnr)
+
+	-- Disable autoformat on certain filetypes
+	if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+		return false
+	end
+
+	-- Disable with a global or buffer-local variable
+	if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+		return false
+	end
+
+	-- disable autoformat for files larger than 128KB
+	if vim.fn.getfsize(vim.fn.expand('%')) > 128 * 1024 then
+		return false
+	end
+
+	-- Disable autoformat for minified files
+	if bufname:match('.min.') then
+		return false
+	end
+
+	-- Disable autoformat for files in a certain path
+	if bufname:match('/node_modules/') then
+		return false
+	end
+
+	return autoformat_opts
+end
+
 conform.setup({
 	formatters_by_ft = {
 		lua = { 'stylua' },
@@ -41,7 +72,7 @@ conform.setup({
 		python = { 'isort', 'blue' },
 		-- sass = { 'prettier' },
 		scss = { 'prettier' },
-		js = { 'prettier' },
+		javascript = { 'standardjs' },
 		json = { 'prettier' },
 		sh = { 'beautysh' },
 		yaml = { 'prettier' },
@@ -51,23 +82,5 @@ conform.setup({
 	formatters = formatters,
 
 	log_level = vim.log.levels.WARN,
-	format_on_save = function(bufnr)
-		-- Disable autoformat on certain filetypes
-		if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-			return
-		end
-
-		-- Disable with a global or buffer-local variable
-		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-			return
-		end
-
-		-- Disable autoformat for files in a certain path
-		local bufname = vim.api.nvim_buf_get_name(bufnr)
-		if bufname:match('/node_modules/') then
-			return
-		end
-
-		return autoformat_opts
-	end,
+	format_on_save = format_on_save,
 })
--- lua/plugins/config/_conform/rubocop.lua
+++ lua/plugins/config/_conform/rubocop.lua
@@ -6,13 +6,14 @@ local function server_argument()
 	end
 end
 
-local function config_path()
-	if vim.fn.filereadable(vim.fn.getcwd() .. '/.rubocop.yml') then
-		return vim.fn.getcwd() .. '/.rubocop.yml'
-	else
-		return vim.fn.expand('$XDG_CONFIG_HOME/rubocop/rubocop.yml')
-	end
-end
+-- TODO: Add ignorelist for projects with incompatible rubocop config
+-- local function config_path()
+-- 	if vim.fn.filereadable(vim.fn.getcwd() .. '/.rubocop.yml') then
+-- 		return vim.fn.getcwd() .. '/.rubocop.yml'
+-- 	else
+-- 		return vim.fn.expand('$XDG_CONFIG_HOME/rubocop/rubocop.yml')
+-- 	end
+-- end
 
 local function condition()
 	return vim.fn.executable('bundle') > 0
@@ -28,7 +29,8 @@ return {
 		'quiet',
 		'--force-exclusion',
 		'--config',
-		config_path(),
+		vim.fn.expand('$XDG_CONFIG_HOME/rubocop/rubocop.yml'),
+		-- config_path(),
 		server_argument(),
 		'--stderr',
 		'--stdin',
