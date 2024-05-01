427d006e312976f67f363ca6611c55946b2d5bd1
Vlada <vladislav.trefil@protonmail.com>
2024-05-02 01:48:28 +0200

    fix(nvim): conform wrong args

--- lua/plugins/config/_conform/stylua.lua
+++ lua/plugins/config/_conform/stylua.lua
@@ -1,15 +1,15 @@
-local function config_path()
-	if vim.fn.filereadable(vim.fn.getcwd() .. '/stylua.toml') == 0 then
-		return {
-			'--config-path',
-			vim.fn.expand('$XDG_CONFIG_HOME/stylua/stylua.toml'),
-		}
-	end
+local root = require('conform.util').root_file({ '.git' })
+local args = {}
+
+-- Use global config if local config does not exist
+if vim.fn.filereadable(vim.fn.getcwd() .. '/stylua.toml') == 0 then
+	args = vim.list_extend(args, {
+		'--config-path',
+		vim.fn.expand('$XDG_CONFIG_HOME/stylua/stylua.toml'),
+	})
 end
 
 return {
-	prepend_args = {
-		config_path(),
-	},
-	cwd = require('conform.util').root_file({ '.git' }),
+	prepend_args = args,
+	cwd = root,
 }
